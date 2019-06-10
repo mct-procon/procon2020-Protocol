1d78493a2ef17146fe450a4e9f88d3c88a77eb11
j1618 <j1618@matsue-ct.jp>
2019-06-10 10:48:12 +0900

    Rename Unsafe4Array -> Unsafe8Array [Improve] Point is smaller.

--- MCTProcon30Protocol/MCTProcon30Protocol/Point.cs
+++ MCTProcon30Protocol/MCTProcon30Protocol/Point.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using MessagePack;
 using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace MCTProcon30Protocol
 {
@@ -10,27 +11,29 @@ namespace MCTProcon30Protocol
     /// x-y平面座標での場所を示す構造体
     /// </summary>
     [MessagePackObject]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
     public struct Point
     {
         /// <summary>
-        /// x座標（ushort分しか使わない）
+        /// x座標
         /// </summary>
         [Key(0)]
-        public uint X { get; set; }
+        public byte X { get; set; }
 
         /// <summary>
-        /// y座標（ushort分しか使わない）
+        /// y座標
         /// </summary>
         [Key(1)]
-        public uint Y { get; set; }
+        public byte Y { get; set; }
 
-        public Point(uint x, uint y)
+        public Point(byte x, byte y)
         {
-            X = x; Y = y;
+            this.X = x;
+            this.Y = y;
         }
 
         public override int GetHashCode()
-            => (int)(X << 16) + (int)Y;
+            => X | (Y << 8);
 
         public override bool Equals(object obj)
         {
@@ -40,31 +43,31 @@ namespace MCTProcon30Protocol
                 return false;
         }
 
-        public static bool operator ==(Point x, Point y) => x.X == y.X && x.Y == y.Y;
-        public static bool operator !=(Point x, Point y) => x.X != y.X || x.Y != y.Y;
+        public static bool operator ==(Point x, Point y) => x.GetHashCode() == y.GetHashCode();
+        public static bool operator !=(Point x, Point y) => x.GetHashCode() != y.GetHashCode();
 
         public static Point operator +(Point x, (int x, int y) y)
         {
-            x.X = (uint)((int)x.X + y.x);
-            x.Y = (uint)((int)x.Y + y.y);
+            x.X = (byte)((int)x.X + y.x);
+            x.Y = (byte)((int)x.Y + y.y);
             return x;
         }
 
         public static Point operator -(Point x, (int x, int y) y)
         {
-            x.X = (uint)((int)x.X - y.x);
-            x.Y = (uint)((int)x.Y - y.y);
+            x.X = (byte)((int)x.X - y.x);
+            x.Y = (byte)((int)x.Y - y.y);
             return x;
         }
 
-        public static Point operator +(Point x, (uint x, uint y) y)
+        public static Point operator +(Point x, (byte x, byte y) y)
         {
             x.X += y.x;
             x.Y += y.y;
             return x;
         }
 
-        public static Point operator -(Point x, (uint x, uint y) y)
+        public static Point operator -(Point x, (byte x, byte y) y)
         {
             x.X -= y.x;
             x.Y -= y.y;
---
+++ MCTProcon30Protocol/MCTProcon30Protocol/Unsafe8Array.cs
@@ -0,0 +1,68 @@
+﻿using MessagePack;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace MCTProcon30Protocol
+{
+    [MessagePackObject]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public unsafe struct Unsafe8Array<T> where T : unmanaged
+    {
+        public T Agent1;
+        public T Agent2;
+        public T Agent3;
+        public T Agent4;
+        public T Agent5;
+        public T Agent6;
+        public T Agent7;
+        public T Agent8;
+        public uint Count {
+            get; private set;
+        }
+
+        public unsafe T this[int index] {
+            get {
+#if DEBUG
+                if (Count >= (uint)index) throw new IndexOutOfRangeException();
+#endif
+                T* ary = (T*)Unsafe.AsPointer(ref this);
+                return ary[index];
+            }
+            set {
+#if DEBUG
+                if (Count >= (uint)index) throw new IndexOutOfRangeException();
+#endif
+                T* ary = (T*)Unsafe.AsPointer(ref this);
+                ary[index] = value;
+            }
+        }
+
+        public static Unsafe8Array<T1> Create<T1>(IReadOnlyList<T1> agents) where T1 : unmanaged {
+#if DEBUG
+            if ((uint)(agents.Count - 1) > 7) throw new ArgumentOutOfRangeException();
+#endif
+            Unsafe8Array<T1> result = new Unsafe8Array<T1>();
+            result.Count = (uint)agents.Count;
+            int i = 0;
+            for (; i < agents.Count; ++i)
+                result[i] = agents[i];
+            return result;
+        }
+
+        public static Unsafe8Array<T1> Create<T1>(params T1[] agents) where T1 : unmanaged
+        {
+#if DEBUG
+            if ((uint)(agents.Length - 1) > 7) throw new ArgumentOutOfRangeException();
+#endif
+            Unsafe8Array<T1> result = new Unsafe8Array<T1>();
+            result.Count = (uint)agents.Length;
+            int i = 0;
+            for (; i < agents.Length; ++i)
+                result[i] = agents[i];
+            return result;
+        }
+    }
+}
