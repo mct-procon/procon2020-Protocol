641826882ffa8bbc8e13bfa06105a20afbdb95e7
j1618 <j1618@matsue-ct.jp>
2019-06-23 17:56:05 +0900

    [ADD] Implement Decision methods.(=, !=, GetHashCode, Equals, ToString)

--- MCTProcon30Protocol/MCTProcon30Protocol/Decision.cs
+++ MCTProcon30Protocol/MCTProcon30Protocol/Decision.cs
@@ -1,6 +1,7 @@
 ﻿using MessagePack;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace MCTProcon30Protocol
@@ -8,8 +9,13 @@ namespace MCTProcon30Protocol
     [MessagePackObject]
     public class Decision
     {
+        private Unsafe8Array<VelocityPoint> agents;
+        
         [Key(0)]
-        public Unsafe8Array<VelocityPoint> Agents { get; set; }
+        public Unsafe8Array<VelocityPoint> Agents {
+            get => agents;
+            set => agents = value;
+        }
 
         [Key(1)]
         public int Score { get; set; }
@@ -25,35 +31,48 @@ namespace MCTProcon30Protocol
         // DO NOT ERASE
         public Decision() { }
 
-        // TODO: ToString
+        public override unsafe bool Equals(object obj)
+        {
+            Decision other = obj as Decision;
+            if (other is null) return false;
+            if (other.GetHashCode() != this.GetHashCode()) return false;
+            long* a = (long*)Unsafe.AsPointer(ref agents);
+            long* b = (long*)Unsafe.AsPointer(ref other.agents);
+            return (a[0] == b[0]) && (a[1] == b[1]);
+        }
 
-        //public override bool Equals(object obj)
-        //{
-        //    Decision other = obj as Decision;
-        //    if (other is null) return false;
-        //    return this.MeAgent1 == other.MeAgent1 && this.MeAgent2 == other.MeAgent2;
-        //}
+        public override unsafe int GetHashCode()
+        {
+            int* start = (int*)Unsafe.AsPointer(ref agents);
+            return start[0] ^ start[1] ^ start[2] ^ start[3];
+        }
 
-        //public override int GetHashCode()
-        //{
-        //    return MeAgent1.GetHashCode() | (MeAgent2.GetHashCode() << 16);
-        //}
+        /// <summary>
+        /// Compare Equality. NOTICE: This doesn't compare Score.
+        /// </summary>
+        public static unsafe bool operator ==(Decision x, Decision y)
+        {
+            if ((x is null) && (y is null)) return true;
+            if (x is null) return false;
+            if (y is null) return false;
+            long* a = (long*)Unsafe.AsPointer(ref x.agents);
+            long* b = (long*)Unsafe.AsPointer(ref y.agents);
+            return (a[0] == b[0]) && (a[1] == b[1]);
+        }
 
-        //public static bool operator ==(Decision x, Decision y)
-        //{
-        //    if ((x is null) && (y is null)) return true;
-        //    if (x is null) return false;
-        //    if (y is null) return false;
-        //    return x.MeAgent1 == y.MeAgent1 && x.MeAgent2 == y.MeAgent2;
-        //}
-        //public static bool operator !=(Decision x, Decision y)
-        //{
-        //    if ((x is null) && (y is null)) return false;
-        //    if (x is null) return true;
-        //    if (y is null) return true;
-        //    return x.MeAgent1 != y.MeAgent1 || x.MeAgent2 != y.MeAgent2;
-        //}
+        /// <summary>
+        /// Compare Equality. NOTICE: This doesn't compare Score.
+        /// </summary>
+        public static unsafe bool operator !=(Decision x, Decision y)
+        {
+            if ((x is null) && (y is null)) return false;
+            if (x is null) return true;
+            if (y is null) return true;
+            long* a = (long*)Unsafe.AsPointer(ref x.agents);
+            long* b = (long*)Unsafe.AsPointer(ref y.agents);
+            return (a[0] != b[0]) || (a[1] != b[1]);
+        }
 
-        //public override string ToString() => $"Agent1 = {MeAgent1}, Agent2 = {MeAgent2}, Score = {Score}";
+        public override string ToString() => $"Agent1 = {agents.Agent1}, Agent2 = {agents.Agent2}, Agent3 = {agents.Agent3}, Agent4 = {agents.Agent4}, Agent5 = {agents.Agent5}, Agent6 = {agents.Agent6}, Agent7 = {agents.Agent7}, Agent8 = {agents.Agent8}, Score = {Score}";
     }
 }
