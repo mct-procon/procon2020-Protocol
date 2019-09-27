cf802a7dadb54248789512f553cbe29f27c8fd1a
mct-gosuzuki <j1618@matsue-ct.jp>
2019-09-27 15:41:17 +0900

    [ADD] Added Ping Json Schema.

---
+++ MCTProcon30Protocol/MCTProcon30Protocol/Json/Ping.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace MCTProcon30Protocol.Json
+{
+    public class Ping
+    {
+        [DataMember(Name = "status")]
+        public string Status { get; set; }
+    }
+}
