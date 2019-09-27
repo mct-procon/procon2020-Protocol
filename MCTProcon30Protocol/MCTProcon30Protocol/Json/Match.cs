614535118a29038b71308854568d0d909e905682
mct-gosuzuki <j1618@matsue-ct.jp>
2019-09-27 15:31:36 +0900

    Added Match Json Schema.

---
+++ MCTProcon30Protocol/MCTProcon30Protocol/Json/Match.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace MCTProcon30Protocol.Json
+{
+    public class Match
+    {
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+
+        [DataMember(Name = "intervalMillis")]
+        public int IntervalMilliseconds { get; set; }
+
+        [DataMember(Name = "matchTo")]
+        public string MatchTo { get; set; }
+
+        [DataMember(Name = "teamID")]
+        public int TeamId { get; set; }
+
+        [DataMember(Name = "turnMills")]
+        public int TurnMilliseconds { get; set; }
+
+        [DataMember(Name = "turns")]
+        public int Turns { get; set; }
+    }
+}
